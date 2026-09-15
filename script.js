7650dc370f681aa5d79c30e2b7d350ae09bcad46
Marcelocavalcante73 <cavalcantemarcelo73@gmail.com>
2026-09-14 22:09:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-function mostrarMensagem() {
-    document.getElementById("mensagem").innerHTML =
-        "🎉 Você acabou de aprender JavaScript!";
+function mudarCor() {
+    document.body.style.backgroundColor = "blue";
 }
